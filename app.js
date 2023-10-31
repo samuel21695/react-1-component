4ae5aafdeb23c315e479addba0303a967965b467
samuel21695 <samuel21695@gmail.com>
2023-10-31 22:54:31 +0900

    🚀feat: function component
    also add for in statement

--- app.js
+++ app.js
@@ -14,3 +14,14 @@
  * ]);
  * 
  */
+
+function component(elementNode, attributes, children) {
+
+  // HTML Element 문자열로 '조립(assemble)' 하는 패턴
+  let elementStr = `<${elementNode}`;
+  for (let key in attributes) { // 객체의 키(key)를 배열처럼 순회한다.
+    // 매개변수 attributes는 객체여야 for in 문을 사용할 수 있다.
+    // 대표적으로 python 에서는 해당 문법 접근이 기본 반복문이다.
+    elementStr += `${key}="${attributes[key]}"`;
+  }
+}
