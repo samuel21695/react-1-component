cf9ba60dcaa0e9ec1b0b947ffc207da37fb6c94c
samuel21695 <samuel21695@gmail.com>
2023-10-31 22:47:10 +0900

    💬 comments: JSDoc comments

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * 에디터에 드디어 내장된 JSDoc 주석 작성 방식으로 아래의 함수를 설명한다.
+ * 
+ * @param {string} elementNode
+ * @param {object} attributes
+ * @param {array} children
+ * @returns string
+ * 
+ * @example
+ * componet(
+ *  'div',
+ *  { style: 'color:blue;' },
+ *  [component('h1', {}, ['This is Page 1'])
+ * ]);
+ * 
+ */
